583f4a6cb1364cd7993770db7a0d778cc8a8ac79
JEnderStar <jayver_cuevas@yahoo.com>
2022-10-23 15:41:52 +0800

    shooting2

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,27 +1,51 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GunScript : MonoBehaviour
 {
+
+
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
-    public Camera fpsCam;
+    public Camera fpscamera;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
-
     {
-        if (Input.GetButtonDown("Fire1"))
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
+
     }
+
     void Shoot()
     {
+
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpscamera.transform.position, fpscamera.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
+            UnityEngine.Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
         }
 
     }

289938b000b66b91447d9085c61c4d7b24975630
yobieesnezhinka <115245997+yobieesnezhinka@users.noreply.github.com>
2022-10-23 15:35:40 +0800

    Relocated Asset Folders to Parent Folders

--- Assets/GAME SCRIPTS/Scripts/VR_Tags/VR_TagEnum.cs
+++
@@ -1,1 +0,0 @@
-namespace VRShooterKit{ public enum VR_TagsEnum{Shotgun_InfiniteBullets,Revolver,Uzi_InifiniteBullets,Bow,Sword,Uzi_Realistic,Uzi_UI,Shotgun_UI,Shotgun_Bullet,Rocket,RocketLauncher,Uzi_Mag,Grenade,LaserSight}}
